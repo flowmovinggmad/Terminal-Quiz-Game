3016cf4faed6e1ed30733262e7d66ec9dfe9f7e6
Hameem Khair <hameemkhairinaz@gmail.com>
2025-02-04 17:55:39 +0600

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,43 @@
+questions = ("How many elements are there in the periodic table?: ",
+             "Which animal lays the largest eggs?: ",
+             "What is the most abundant gas in Earth's atmosphere?: ",
+             "How many bones are there in the human body?: ",
+             "Which planet in the solar system is the hottest?: ")
+
+options = (("A. 116", "B. 117", "C. 118", "D. 119"),
+           ("A. Whale", "B. Crocodile", "C. Elephant", "D. Ostrich"),
+           ("A. Nitrogen", "B. Oxygen", "C. Carbon-Dioxide", "D. Hydrogen"),
+           ("A. 206", "B. 207", "C. 208", "D. 209"),
+           ("A. Mercury", "B. Venus", "C. Mars", "D. Earth"))
+
+answers = ("C", "D", "A", "A", "B")
+guesses = []
+score = 0
+question_num = 0
+
+for question in questions:
+    print('--------------------')
+    print(question)
+
+    for option in options[question_num]:
+        print(option)
+
+    guess = input("Enter (A, B, C or D): ").upper()
+    guesses.append(guess)
+
+    if guess == answers[question_num]:
+        score += 1
+        print("Correct.")
+    else:
+        print(f"{answers[question_num]} is the correct answer.")
+
+
+    question_num += 1
+
+print()
+print("------------------------")
+print("        RESULT")
+print("------------------------")
+print()
+
+print(f"Your score: {score} out of {len(answers)}")
